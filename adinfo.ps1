f6fd5e659cb43cf330232516cd2861e24dc3e8f8
Ahmed Bayoumy <v-abayoumy@microsoft.fom>
2021-09-28 10:24:13 +0200

    update font

--- adinfo.ps1
+++ adinfo.ps1
@@ -4,45 +4,67 @@ Title: adinfo.PS1
 Author: Ahmed Bayoumy
 Category: Utility Script
 Description:
-Generate HTML report for AD
+Generate HTML report for AD Info
 #>
 
 #CSS codes
 $header = @"
 <style>
+  div {
+    line-height: normal;
+  }
+
     body {
-        color:#fff;background-color:#16a085;
+        color:#ffffff;
+        background-color:#2E4057;
+        line-height: normal;
     }
 
     h1 {
 
-        font-family: Arial, Helvetica, sans-serif;
-        color: #fff;
+        font-family: Open Sans;
+        color: #ffffff;
         font-size: 28px;
     }
 
     
     h2 {
 
-        font-family: Arial, Helvetica, sans-serif;
-        color: #fff;
+        font-family: Open Sans;
+        color: #ffffff;
         font-size: 16px;
 
     }
 
+    h3 {
+
+        font-family: Open Sans;
+        color: #ffffff;
+        font-size: 12px;
+        line-height: normal;
+
+    }
+
+    h4 {
+
+        font-family: Open Sans;
+        color: #ffffff;
+        font-size: 10px;
+        line-height: normal;
+    }
+
     a {
 
-        font-family: Arial, Helvetica, sans-serif;
-        color: #fff;
+        font-family: Open Sans;
+        color: #ffffff;
         font-size: 16px;
-
     }
     
     
    table {
 		font-size: 12px;
 		border: 0px; 
-		font-family: Arial, Helvetica, sans-serif;
+		font-family: Open Sans;
 	} 
 	
     td {
@@ -54,7 +76,7 @@ $header = @"
     th {
         background: #395870;
         background: linear-gradient(#49708f, #293f50);
-        color: #fff;
+        color: #ffffff;
         font-size: 11px;
         text-transform: uppercase;
         padding: 10px 15px;
@@ -69,7 +91,7 @@ $header = @"
 
     #CreationDate {
 
-        font-family: Arial, Helvetica, sans-serif;
+        font-family: Open Sans;
         color: black;
         font-size: 12px;
         text-align: left;
@@ -85,31 +107,39 @@ if (!(New-Object Security.Principal.WindowsPrincipal([Security.Principal.Windows
     Exit
 }
 
-# check and load RSAT-AD-PowerShell
+# check RSAT-AD-PowerShell
 if( ! (get-module -list activedirectory)){
     Write-Warning "Please run this script on AD DC."
     Start-Process "https://docs.microsoft.com/en-us/troubleshoot/windows-server/system-management-components/remote-server-administration-tools"
     Exit    
 }
+# $DomainRole = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty DomainRole
+# Standalone Workstation (0)
+# Member Workstation (1)
+# Standalone Server (2)
+# Member Server (3)
+# Backup Domain Controller (4)
+# Primary Domain Controller (5)
+
+
 
 Import-Module activedirectory
 Import-Module grouppolicy
+$Forest=Get-ADForest
+$Domain=Get-ADDomain
 $LogPath = "$($PSScriptRoot)\AD-Info $((Get-Date).ToString('dd-MM-yyyy'))"
 $zipFile = "$($LogPath).zip"
-$HostName = [System.Net.Dns]::GetHostName()
+$HostName = $env:computername
 $Now=$((Get-Date).ToString('dd-MM-yyyy hh-mm'))
+$ReportFile = "$($LogPath)\$($Domain.Name)-$($Now).html"
+
 If (!(Test-Path -Path $LogPath -ErrorAction SilentlyContinue )) {  New-Item $LogPath -Type Directory -ErrorAction SilentlyContinue | Out-Null }
 
-# $DFL=(Get-ADDomain).DomainMode
-# $FFL=(Get-ADForest).ForestMode
-$Forest=Get-ADForest
-# $Domain=Get-ADDomain
-Write-Output "Forest:$($Forest.Name) $($Forest.Domains)"
-Get-ADDomain | Format-List Name, DomainMode > "$LogPath\DFL.txt"
-Get-ADForest | Format-List Name, ForestMode > "$LogPath\FFL.txt"
+Write-Output "Forest:$($Forest.Name) - $($Forest.ForestMode) `r`nDomain:$($Domain.Name) - $($Domain.DomainMode) "
+Get-ADDefaultDomainPasswordPolicy > "$LogPath\PasswordPolicy.txt"
 Netdom /query fsmo > "$($LogPath)\fsmo.txt"
 Repadmin /showrepl * /csv > "$LogPath\showrepl-$($Now).csv"
-Gpresult /h "$LogPath\GPResult-$($HostName)-$($Now).html"
+Gpresult /f /h "$LogPath\GPResult-$($HostName)-$($Now).html"
 Get-ADDomainController -Filter * | Select-Object Name, OperatingSystem, IPv4Address, Site > "$LogPath\dclist.txt"
 (get-ADForest).domains | ForEach-Object { get-GPO -all -Domain $_ | Select-Object @{n='Domain Name';e={$_.DomainName}}, @{n='GPO Name';e={$_.DisplayName}}, @{n='GPO Guid';e={$_.Id}} , @{n='Gpo Status';e={$_.GpoStatus}} , @{n='Creation Time';e={$_.CreationTime}} , @{n='Modification Time';e={$_.ModificationTime}} } | Export-Csv "$LogPath\AllGPOsList.csv"
 Get-GPOReport -All  -ReportType HTML -Path "$LogPath\GPOReport-$($Now).html"
@@ -127,6 +157,11 @@ foreach ($Site in $Sites) {
  )            
 }
 $obj | Export-Csv "$LogPath\sites-$($Now).csv" -NoType 
+
+$Report="<h2>Forest:$($Forest.Name) FL:$($Forest.ForestMode)</h2><h3>DomainNamingMaster:$($Forest.DomainNamingMaster)<br>SchemaMaster:$($Forest.SchemaMaster)</h3></h2>Domain:$($Domain.Name) FL:$($Domain.DomainMode) </h2>" 
+
+#$Report = ConvertTo-HTML -Body "$ComputerName $OSinfo $ComputerModel $RAMInfo $DiscInfo " -Head $header -Title "$($Domain.DomainMode) Report" -PostContent "<p id='CreationDate'> Creation Date: $(Get-Date)</p>"
+ConvertTo-HTML -Body "$Report" -Head $header -Title "$($Domain.Name) Report" -PostContent "<h4> Created @ $(Get-Date)</h4>" | Out-File $ReportFile
 
 Compress-Archive -Path $LogPath -DestinationPath $zipFile -Update
 
